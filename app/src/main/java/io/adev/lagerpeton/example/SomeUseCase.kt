30d92cd35e33e4c26309f040af6d4c5018170ee4
kirill.terekhov <kirill.terekhov@alibaba-inc.com>
2020-12-14 10:25:17 +0300

    Fix typo

--- app/src/main/java/io/adev/lagerpeton/example/SomeUseCase.kt
+++ app/src/main/java/io/adev/lagerpeton/example/SomeUseCase.kt
@@ -5,7 +5,7 @@ import io.adev.lagerpeton.Lager
 class SomeUseCase {
 
     fun execute(arg1: String, arg2: Boolean, l: Lager) {
-        l.error("do not name args 'arg1' and 'arg2'") {
+        l.error("do not call args 'arg1' and 'arg2'") {
             it.put("arg1", arg1)
             it.put("arg2", arg2)
         }
