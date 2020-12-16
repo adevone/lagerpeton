aaf9b461f464987ea77419da968846b43c0db73a
kirill.terekhov <kirill.terekhov@alibaba-inc.com>
2020-12-16 05:22:11 +0300

    Update android example

--- app/src/main/java/io/adev/lagerpeton/example/App.kt
+++ app/src/main/java/io/adev/lagerpeton/example/App.kt
@@ -1,17 +1,14 @@
 package io.adev.lagerpeton.example
 
 import android.app.Application
-import io.adev.lagerpeton.AndroidCollector
-import io.adev.lagerpeton.PrimitivesOnlyAccumulator
-import io.adev.lagerpeton.TypedLager
+import io.adev.lagerpeton.Lager
+import io.adev.lagerpeton.android
 
 class App : Application() {
 
     override fun onCreate() {
         super.onCreate()
-        ServicesLocator.globalLogger = TypedLager.create(
-            AndroidCollector, PrimitivesOnlyAccumulator
-        ) {
+        ServicesLocator.globalLogger = Lager.android {
             it.put("versionName", BuildConfig.VERSION_NAME)
             it.put("versionCode", BuildConfig.VERSION_CODE)
             it.put("appName", getString(R.string.app_name))
